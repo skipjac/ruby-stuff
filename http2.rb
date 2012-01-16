b9ee5205238a6d587de1579027a49769b7979f02
Skip Moore <skipjack@skipjack.info>
2012-01-16 14:19:58 -0800

    searches Zendesk and returns a JSON results

---
+++ http2.rb
@@ -0,0 +1,38 @@
+require 'pp'
+require 'net/https'
+require 'crack'
+require 'json'
+
+http = Net::HTTP.new('skipjack.zendesk.com', 443)
+http.use_ssl = true
+count = 1
+newJSON = 'test'
+begin
+http.start do |http|
+   req = Net::HTTP::Get.new('/search.json?query=skip&page=' + count.to_s)
+
+   # we make an HTTP basic auth by passing the
+   # username and password
+   req.basic_auth 'skip@something.net', 'password'
+   resp, data = http.request(req)
+   newJSON = JSON.parse(data)
+   
+   if newJSON.length > 0
+      # pp newJSON[0]
+      newJSON.each do |test|
+        if test['email']
+          pp test
+        end
+        
+      end
+      print newJSON.length.to_s + '
+      '
+    else
+      print 'none found
+      '
+      print count.to_s + '
+      '
+    end
+    count += 1
+ end
+end while newJSON.length > 0
