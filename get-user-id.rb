1f3989a79e665a78dce288e004f03163cc5c650f
Skip Linux <skipjack@skipjack.info>
2012-01-16 17:29:00 -0800

    made a method?

---
+++ get-user-id.rb
@@ -0,0 +1,30 @@
+require 'pp'
+require 'net/https'
+require 'crack'
+require 'json'
+
+def skippy (email)
+    http = Net::HTTP.new('skipjack.zendesk.com', 443)
+    http.use_ssl = true
+    
+    http.start do |http|
+       req = Net::HTTP::Get.new('/people.json?query=' + email)
+    
+       # we make an HTTP basic auth by passing the
+       # username and password
+       req.basic_auth 'skip@techassistant.net', 'password'
+       resp, data = http.request(req)
+       newJSON = JSON.parse(data)
+       
+       if newJSON.length > 0
+            pp newJSON
+            newJSON.each do |userID|
+                print userID['id']
+                end
+        else
+          print 'none found
+          '
+        end
+     end
+end
+skippy('bob@test.com')
